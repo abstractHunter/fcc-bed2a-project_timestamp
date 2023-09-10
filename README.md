f75674d93a9fa6e73f56329d23f948baa0855efc
milkiyd <milkiyd@bugger.com>
2023-09-10 19:29:13 +0000

    Complete project timestamp

---
+++ README.md
@@ -0,0 +1,10 @@
+# Timestamp Microservice
+
+This is the code for the Timestamp Microservice project from FreeCodeCamp back end developement and apis certification. The project is hosted on replit at
+
+It is a simple API that returns a JSON object with a Unix timestamp and a UTC timestamp.  The API accepts a date string as a parameter and returns the JSON object with the timestamp data.  If the date string is empty, the API returns the current timestamp.
+Built with Node.js and Express.js.
+
+The boilerplate can be found at https://github.com/freeCodeCamp/boilerplate-project-timestamp/
+
+Instructions for building your project can be found at https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/timestamp-microservice
---
+++ index.js
@@ -0,0 +1,66 @@
+// index.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: 'hello API' });
+});
+
+
+// GET: /api/:date?
+app.get("/api/:date?", function (req, res) {
+  let date;
+
+  // get date from request
+  let reqDate = req.params.date;
+
+  // if date is empty, set date to current date
+  if (!reqDate) {
+    date = new Date();
+  }
+
+  // if date is a number in Unix format, convert to date
+  if (!isNaN(reqDate)) {
+    date = new Date(parseInt(reqDate));
+  }
+
+  // if date is a string, convert to date
+  if (isNaN(date)) {
+    date = new Date(reqDate);
+  }
+
+  // if date is invalid, return error
+  if (isNaN(date)) {
+    res.json({ error: "Invalid Date" });
+  }
+
+  // return date in JSON format
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
+});
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
